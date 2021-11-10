804f2e426d65711ceeb9534aa39cb54e4a7b4a5b
geekbrains <info@geekbrains.ru>
2021-11-10 20:28:24 +0300

    bug fix

--- 2_5.py
+++ 2_5.py
@@ -1,19 +1,19 @@
 # 2.5
 
-price_list = [] # список для цен на товары
+price_list = [34.64, 268.4, 76, 35.98, 545.64, 43, 7.54, 43.64, 21,9 ] # список для цен на товары
 new_price_list = [] # новый список для цен по убыванию
 top_expensive = [] # новый список для 5 самых дорогих цен по возрастанию
 
 text = [] # список для вывода текста
 
-for i in range(1, 21): #
-    prices = float(input('Введите цену каждого товара в руб. и коп.: ')) # вводим числа
-    price_list.append(prices) # добавим числа в списку
-    new_price_list.append(prices)
-    top_expensive.append(prices)
+for i in range(len(price_list)): #
+    # prices = float(input('Введите цену каждого товара в руб. и коп.: ')) # вводим числа, если хотим создать вручную
+    # price_list.append(prices) # добавим числа в списку
+    new_price_list.append(price_list[i])
+    top_expensive.append(price_list[i])
 
-    price_rub = round(prices // 1) # выводим числа перед запятой
-    price_penny = round(prices % 1 * 100) # выводим числа после запятой
+    price_rub = round(price_list[i] // 1) # выводим числа перед запятой
+    price_penny = round(price_list[i] % 1 * 100) # выводим числа после запятой
 
     text.append([price_rub, price_penny]) # добавим оба числа в отдельный список в списке
 
